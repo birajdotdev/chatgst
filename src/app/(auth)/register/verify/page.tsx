1aa2c0eb3c75692343aca2a6c71338f78d9723d4
Biraj Shrestha <birajdotdev@gmail.com>
2025-11-16 20:14:58 +0545

    ✨ feat(auth): wrap VerifyOtpView in Suspense for improved loading state handling

--- src/app/(auth)/register/verify/page.tsx
+++ src/app/(auth)/register/verify/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
+
+import { Spinner } from "@/components/ui/spinner";
 import { VerifyOtpView } from "@/modules/auth/views/verify-otp-view";
 
 export default function Page() {
-  return <VerifyOtpView />;
+  return (
+    <Suspense fallback={<Spinner className="size-12 text-primary" />}>
+      <VerifyOtpView />
+    </Suspense>
+  );
 }
