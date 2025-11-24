999fc24b448948a09d3a131dcfc07cbc1767b585
Biraj Shrestha <birajdotdev@gmail.com>
2025-11-24 17:04:42 +0545

    ✨ feat: Update ChatLayout and ChatView components to ensure proper overflow handling

--- src/modules/chat/views/chat-view.tsx
+++ src/modules/chat/views/chat-view.tsx
@@ -6,10 +6,12 @@ import { useDefaultChat } from "@/modules/chat/components/default-chat-context";
 export function ChatView() {
   const { chat } = useDefaultChat();
   return (
-    <ChatBot
-      chat={chat}
-      className="max-w-5xl"
-      aiPromptInputClassName="max-w-3xl"
-    />
+    <section className="h-full flex-1 overflow-hidden">
+      <ChatBot
+        chat={chat}
+        className="max-w-5xl"
+        aiPromptInputClassName="max-w-3xl"
+      />
+    </section>
   );
 }
