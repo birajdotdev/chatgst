5b8c1cb0ee4c9538887da51def9e14e1584f53a1
Biraj Shrestha <birajdotdev@gmail.com>
2026-01-06 14:25:57 +0545

    ✨ refactor: remove unnecessary comments and simplify page component signatures

--- src/app/(home)/general/page.tsx
+++ src/app/(home)/general/page.tsx
@@ -1,8 +1,5 @@
 import { GeneralChatView } from "@/modules/home/views/general-chat-view";
 
-// Auth redirect handled by middleware (src/proxy.ts)
-// Authenticated users are redirected to /chat
-
 export default function Page() {
   return <GeneralChatView />;
 }
--- src/app/(protected)/chat/[id]/page.tsx
+++ src/app/(protected)/chat/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { DefaultChatProvider } from "@/modules/chat/components/default-chat-context";
 import { ChatView } from "@/modules/chat/views/chat-view";
 
-export default function Page(props: PageProps<"/chat/[id]">) {
+export default function Page() {
   return (
     <DefaultChatProvider>
       <ChatView />
